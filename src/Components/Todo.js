ffe7eb469c5cfbef851b5c96c152fe860ba9357b
Suvam Chanda <Suvam_Chanda@epam.com>
2021-08-25 10:18:38 +0530

    Update Todo.Js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,12 +1,12 @@
 function ToDo(props) {
     return (
       <div>
-      <div className='card'>
-      <h2>{props.text}</h2> 
-      <div className='actions'>
-      <button className='btn'> Delete </button>
-      </div>
-      </div>
+        <div className='card'>
+          <h2>{props.text}</h2> 
+          <div className='actions'>
+            <button className='btn'> Delete </button>
+           </div>
+        </div>
       </div>
     );
    }
